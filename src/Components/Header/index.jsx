7254bc52786aaac3430bc89eae50a001ccf371ca
RGHuynh <randall.g.huynh@gmail.com>
2018-12-16 00:15:50 -0500

    Add bootstrap

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation'
+import './index.css';
+
+export default function() {
+    return(
+        <header>
+            <Navigation />
+            header
+        </header>
+    )
+}
