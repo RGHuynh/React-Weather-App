07bd98a586942cb661e21e3cbc7318672b42af1a
RGHuynh <randall.g.huynh@gmail.com>
2018-12-17 11:45:38 -0500

    Add underscore to sentence

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import AutosizeInput from 'react-input-autosize';
 import './index.css';
 
 export default class Navigation extends Component {
@@ -20,9 +21,16 @@ export default class Navigation extends Component {
             <section className="section-container">
                 <div className="container">
                     <h1 className="description">Check out </h1>
-                    <form className="description-form">
+                    {/* <form className="description-form">
                         <input className="description-textbox" type="text" placeholder="____" value={this.state.input} onChange={this.handleChange}></input>
-                    </form>
+                    </form> */}
+                    <AutosizeInput 
+                        className="description-form"
+                        name="form-field-location"
+                        value={this.state.input}
+                        onChange={this.handleChange}
+                        placeholder="_________"
+                    />
                     <h1 className="description">it's raining</h1>
                 </div>
             </section>
