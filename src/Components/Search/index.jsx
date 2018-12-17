3f8c186b3ef70576bc5ffb1cfb446ba66a53d5ee
RGHuynh <randall.g.huynh@gmail.com>
2018-12-17 11:29:09 -0500

    wip style

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -5,14 +5,25 @@ export default class Navigation extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            input: "______"
+            input: ""
         };
+        this.handleChange = this.handleChange.bind(this); 
+    }
+
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        });
     }
     render(){
         return(
             <section className="section-container">
                 <div className="container">
-                    <h1 className="description">Check out ______, it's raining</h1>
+                    <h1 className="description">Check out </h1>
+                    <form className="description-form">
+                        <input className="description-textbox" type="text" placeholder="____" value={this.state.input} onChange={this.handleChange}></input>
+                    </form>
+                    <h1 className="description">it's raining</h1>
                 </div>
             </section>
         );
