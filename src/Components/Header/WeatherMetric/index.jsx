9f9e9ea8d82808848b796ffc770d9f8d278cd180
RGHuynh <randall.g.huynh@gmail.com>
2018-12-17 23:12:45 -0500

    Add style change to UI

--- src/Components/Header/WeatherMetric/index.jsx
+++ src/Components/Header/WeatherMetric/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index.css';
 
-export default function WeatherMetric({metric, acronym, title}) {
+export default function WeatherMetric({metric, acronym, title, image}) {
     return(
         <div className="weatherMetric-container">
-            <h6 className="weatherMetric-title">{title}</h6>
+            <img className="weatherMetric-title" src={image}></img>
+            {/* <h6 className="weatherMetric-title">{title}</h6> */}
             <div className="metric-wrapper">
                 <h6 className="metric">{metric}</h6>
                 <h6 className="metric-acronym">{acronym}</h6>
